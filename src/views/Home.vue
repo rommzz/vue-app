a907c208437bd96b62e8556068f5d1b778297863
rommy <rommy_zohara@yahoo.com>
2020-12-03 05:34:43 +0700

    card item

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,50 @@
 <template>
-  <div class="columns is-mobile">
-  <div class="column">1</div>
-  <div class="column">2</div>
-  <div class="column">3</div>
-  <div class="column">4</div>
-</div>
+  <div class="columns is-mobile is-multiline px-6">
+    <div class="column card is-2">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="~@/assets/images/qtela.png" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media is-left">
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="button is-warning">Add to chart</div>
+        </div>
+      </div>
+    </div>
+    <div class="column card is-2">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="~@/assets/images/fernleaf.png" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="button is-warning">Add to chart</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 </template>
 
 <script>

22f7ddc0b66bb333551d7fd9038c0823ca97d9e4
rommy <rommy_zohara@yahoo.com>
2020-12-02 10:04:52 +0700

    box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,10 @@
 <template>
-  <div class="py-1 px-6" >
-    <div class="card">
-      <div class="card-content">
-        <p class="title">
-          “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
-        </p>
-        <p class="subtitle">
-          Jeff Atwood
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Share on <a href="#">Facebook</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-  </div>
-
+  <div class="columns is-mobile">
+  <div class="column">1</div>
+  <div class="column">2</div>
+  <div class="column">3</div>
+  <div class="column">4</div>
+</div>
 </template>
 
 <script>
@@ -29,21 +15,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
